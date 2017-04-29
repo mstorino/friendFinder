9ef66e1c5088c128912124ea5272eb9e9413403a
Maggie <test@apples-MacBook-Pro.local>
2017-04-29 07:39:51 -0500

    add modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -10,7 +10,7 @@ var userArray = [
     photo: "google.com",
     scores: [
 		5,
-		10
+		5
       	
     ]
   },
@@ -19,7 +19,7 @@ var userArray = [
     photo: "google.com",
     scores: [
 		4,
-		6
+		5
     ]
   }
 ];
--- app/public/survey.js
+++ app/public/survey.js
@@ -1,93 +1,97 @@
+// $(document).ready(function(){
+//     $("#myBtn").click(function(){
+//         $("#myModal").modal();
+//     });
+// });
+
+
 $("#submit").on("click", function(event) {
       event.preventDefault();
 
       	var currentUrl = window.location.origin;
-      	var userArray;
+      	// var userArray;
+      
+      //create new friend object with information from user
+	    var newFriend = {
+	        userName: $("#userName").val().trim(),
+	        scores: [
+	            $("#q1Selected").val(), 
+	           	$("#q2Selected").val()
+	        ]
+	     };
+
+		var bestFriend = "";
+		var total = 100;
+		var newTotal;
+
+	// get data from api
 
       	$.ajax({url: currentUrl + "/api/friends", method: "GET" }).done(function(userArray){
       		
-	        //create new friend object with information from user
-	        var newFriend = {
-	            userName: $("#userName").val().trim(),
-	            scores: [
-	            	$("#q1Selected").val(), 
-	            	$("#q2Selected").val()
-	            ]
-	        };
+	        
 
-			var bestFriend = "";
-			var total = 0;
-			var newTotal = 0;
-			var oldFriend = "";
-			var i = 0;
 			apiCounter();
 			
 			function apiCounter(){
-				for (i; i < userArray.length; i++) {
+				for (var i=0; i < userArray.length; i++) {
 						oldFriend = userArray[i];
 						answerCompare();
 				}
+				console.log("Low Score: " + total + ". Your new best friend: " + bestFriend);
+			}
+		
+
+	//functions
+
+		function answerCompare(){
+			newTotal = 0;
+			for (var j = 0; j < newFriend.scores.length; j++) {
+
+				if(oldFriend.scores[j] === newFriend.scores[j]) {
+					newTotal += 0;
+				} else {		
+					newTotal += Math.abs(oldFriend.scores[j] - newFriend.scores[j]);
+				}		
 			}
 
-			function answerCompare(){
-				for (var j = 0; j < newFriend.scores.length; j++) {
-					newTotal = 0;
+			totalCompare();
+		}
 
-					if(oldFriend.scores[j] === newFriend.scores[j]) {
-								// console.log(total);
-						newTotal += 0;
-					} else {
-								
-						newTotal += Math.abs(oldFriend.scores[j] - newFriend.scores[j]);
-						console.log("newTotal" + newTotal + " " + oldFriend.userName);
-						totalCompare();
-					}
-				}
-
+		function totalCompare(){
+			if (newTotal < total) {
+				total = newTotal;
+				bestFriend = oldFriend.userName;
+				console.log("new total is:" + total);
+			} else if (newTotal === total) {
+				bestFriend += " & " + oldFriend.userName;
 			}
-
-			function totalCompare(){
-				if (newTotal > total) {
-					total = newTotal;
-					bestFriend = oldFriend.userName;
-				} else if (newTotal === total) {
-					bestFriend += " & " + oldFriend.userName
-				} 
-			}
-
-
-
-		// for (i; i < userArray.length; i++) {
-		// 			oldFriend = userArray[i];
-					
-		// 			for (var j = 0; j < newFriend.scores.length; j++) {
-
-		// 				newTotal = 0;
-
-		// 				if(oldFriend.scores[j] === newFriend.scores[j]) {
-						
-		// 					newTotal += 0;
-		// 				} else {
-							
-		// 					newTotal += Math.abs(oldFriend.scores[j] - newFriend.scores[j]);
-		// 						console.log("newTotal" + newTotal + " " + oldFriend.userName);
-								
-		// 						if (newTotal > total) {
-		// 							total = newTotal;
-		// 							bestFriend = oldFriend.userName;
-		// 						} else if (newTotal === total) {
-		// 							bestFriend += " & " + oldFriend.userName
-		// 						} 
-		// 				}
-		// 			}
-
-		// 	}
-
-			console.log("High Score: " + total + ". Your new best friend: " + bestFriend);
+		}
 
 		//empty fields
 
 		//post data to friends API
 		$.post("/api/friends", newFriend);
+		
+		$("#myModal").modal('show');
+		$("#myModal").on('shown.bs.modal', function(){
+				$('#myModal').find('.modal-body').append('<p>Friend: ' + bestFriend + '</p>');
+		});
+
+
+
+							
+
+    
+  // });
+
+
+
 	});
+
+//reset values
+
+		$("#userName").val(" ");
+	    $("#q1Selected").val("1"); 
+	    $("#q2Selected").val("1");
+	
 });
